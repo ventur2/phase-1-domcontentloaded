2ea633b108a8ae9a0111f22c200ccf187b4e6567
rachell <dazzlemevanity@gmail.com>
2022-06-13 14:23:53 -0400

    commit tree 2e18e53be6d194fdbf5033c5156f23c32594089e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let p= document.querySelector('p')
+    console.log(p)
+    p.textContent = 'This is really cool!'
+  });
+  
